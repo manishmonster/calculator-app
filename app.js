fe7451b5cbf3118236c60c7db6c555e984d21e4d
manishmonster <manishtwayana@gmail.com>
2021-07-27 16:46:53 +1200

    printing ouput

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var outputText = "";
 var numbers = [];
-var opeartors = [];
+var operators = [];
 var currentnumber = 0;
 var newEntry = false;
 $('input[name="state-d"]').change(function(){
@@ -34,8 +34,8 @@ $('.decimal').click(function(){
 $('.secondary-key').click(function(){
     var func = $(this).val();
     if( func == 'del'){
-        if(opeartors.length == numbers.length){
-            opeartors.pop(opeartors[opeartors.length - 1]);
+        if(operators.length == numbers.length){
+            operators.pop(operators[operators.length - 1]);
         }
         else if(currentnumber.length > 0){
             currentnumber = currentnumber.slice(0, -1);
@@ -53,15 +53,22 @@ $('.secondary-key').click(function(){
 })
 $('.operator').click(function(){
     var opt = $(this).val();
-    if (opeartors.length == numbers.length) {
-        opeartors[opeartors.length - 1] = opt;
+    if (operators.length == numbers.length) {
+        operators[operators.length - 1] = opt;
     }
     else{
-        opeartors.push(opt);
+        operators.push(opt);
     }
     newEntry = true;
     displayOut();
 });
+$('.enter').click(function(){
+    operator();
+});
+$('.reset').click(function(){
+    resetFun();
+    $('.outputs').text(0);
+})
 function lastNumberEdit(){
     numbers[numbers.length - 1] = currentnumber;
 }
@@ -70,58 +77,81 @@ function displayOut(){
     if(numbers.length > 0){
         for(var i = 0; i < numbers.length; i++){
             outputText = outputText + numbers[i];
-            if ( opeartors[i]) {
-                outputText = outputText + opeartors[i];
+            if ( operators[i]) {
+                outputText = outputText + operator_view(operators[i]);
             }
         }
     }
     else{
         outputText = 0;
     }
-    console.log(numbers);
-    console.log(opeartors);
-    console.log(currentnumber);
     $('.outputs').text(outputText);
+}
+function operator_view(operator_){
+    switch (operator_) {
+        case 'add':
+            return ' + ';
+            break;
+        case 'subtract':
+            return ' - ';
+            break;
+        case 'divide':
+            return ' / ';
+            break;
+        case 'multiply':
+            return ' * ';
+            break;
+    }
 }
 function formatingNumbers(digit){
     return digit.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
 }
-function opeartor(){
+function operator(){
     var outputs = 0;
     if(numbers.length > 0){
-        switch (opeartors[0]) {
-            case '+':
+        console.log(operators[0]);
+        switch (operators[0]) {
+            case 'add':
                 outputs = parseFloat(numbers[0]) + parseFloat(numbers[1]);
                 break;
-            case '-':
+            case 'subtract':
                 outputs = parseFloat(numbers[0]) - parseFloat(numbers[1]);
                 break;
-            case '/':
+            case 'divide':
                 outputs = parseFloat(numbers[0]) / parseFloat(numbers[1]);
                 break;
-            case '*':
+            case 'multiply':
                 outputs = parseFloat(numbers[0]) * parseFloat(numbers[1]);
                 break;
         } 
+        console.log(outputs);
         for(var i = 2; i < numbers.length; i++){
             var num =  parseFloat(numbers[i]);
             outputs = parseFloat(outputs);
-            if ( opeartors[i-1]) {
-                switch (opeartors[i-1]) {
-                    case '+':
+            if ( operators[i-1]) {
+                switch (operators[i-1]) {
+                    case 'add':
                         outputs = outputs + num;
                         break;
-                    case '-':
+                    case 'subtract':
                         outputs = outputs - num;
                         break;
-                    case '/':
+                    case 'divide':
                         outputs = outputs / num;
                         break;
-                    case '*':
+                    case 'multiply':
                         outputs = outputs * num;
                         break;
                 }
             }
         }
+        console.log(outputs);
+        $('.outputs').text(outputs);
+        resetFun();
     }
+}
+function resetFun(){
+    numbers = [];
+    currentnumber = 0;
+    operators = [];
 }
